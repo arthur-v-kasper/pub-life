5edeca0a27866edadebc8966a627255a02050254
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-06-22 22:02:58 -0300

    Add request to get pub data

--- app/javascript/components/Pub/Pub.js
+++ app/javascript/components/Pub/Pub.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Pub = () => {
-  return <div>This is the Pubs#show view for our app!</div>
+const Pub = (props) => {
+  const [pub, setPub] = useState({})
+  const [review, setReview] = useState({})
+
+  useEffect(()=>{
+    const slug = props.match.params.slug;
+    const url = `/api/v1/pubs/${slug}`;
+
+    axios.get(url)
+    .then(resp => console.log(resp) )
+    .catch(resp => console.log(resp))
+  },[])
+
+  return (
+    <div className="wrapper">
+      <div className="column">
+        <div className="header"></div>
+        <div className="reviews"></div>
+      </div>
+      <div className="column">
+        <div className="reviews-form">[Reviews form goes here.]</div>
+      </div>
+    </div>
+  )
 }
 
 export default Pub;
