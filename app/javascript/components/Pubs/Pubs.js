8a0b8702105d3b7c9f656af3d98f96e0d9145882
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-06-22 21:15:44 -0300

    Add Pubs styled components

--- app/javascript/components/Pubs/Pubs.js
+++ app/javascript/components/Pubs/Pubs.js
@@ -1,6 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Pub from './Pub';
+import styled from 'styled-components';
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`;
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`;
 
 const Pubs = () => {
   const [pubs, setPubs] = useState([]);
@@ -24,15 +52,15 @@ const Pubs = () => {
   })
   
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenPubs</h1>
-        <div className="subheader">Honest reviews from who loves Pubs</div>
-      </div>
-      <div className="grid">
+        <Subheader>Honest reviews from who loves Pubs</Subheader>
+      </Header>
+      <Grid>
         {grid}
-      </div>
-    </div>
+      </Grid>
+    </Home>
   )
 }
 
